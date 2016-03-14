25e52637196ef5765cb923b52e4fa1a74d7d7b33
xuatz <xzlow10@gmail.com>
2016-03-14 18:57:51 +0800

    indentation

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,41 +1,55 @@
 import React, { Text, View, PropTypes} from 'react-native';
 var Button = require('react-native-button');
 
+let { StyleSheet } = React;
+
 let Footer = React.createClass({
 
-  renderFilter(filter, name) {
-    if (filter === this.props.filter) {
-      return (
-        <Text>
-          {name}
-        </Text>
-      );
-    }
-    return (
-      <Button style={{color: 'blue'}} onPress={() => { this.props.onFilterChange(filter)}}>
-        {name}
-      </Button>
-    )
-  },
+	renderFilter(filter, name) {
+		if (filter === this.props.filter) {
+			return (
+				<Text >
+					{name}
+				</Text>
+			);
+		}
+		return (
+			<Button style={{color: 'blue'}} onPress={() => { this.props.onFilterChange(filter)}}>
+				{name}
+			</Button>
+		)
+	},
 
-  render(){
-    return(
-      <View>
-        {this.renderFilter('SHOW_ALL', 'All')}
-        {this.renderFilter('SHOW_COMPLETED', 'Completed')}
-        {this.renderFilter('SHOW_ACTIVE', 'Active')}
-      </View>
-    );
-  }
+	render(){
+		return(
+			<View style={styles.filterOptions}>
+				{this.renderFilter('SHOW_ALL', 'All')}
+				{this.renderFilter('SHOW_COMPLETED', 'Completed')} 
+				{this.renderFilter('SHOW_ACTIVE', 'Active')}
+			</View>
+		);
+	}
+});
+
+var styles = StyleSheet.create({
+	filterOptions: {
+		paddingTop:20,
+		flexDirection:'row',
+		alignItems:'center',
+		justifyContent:'space-around'
+	},
+	filterItem: {
+		padding:10
+	}
 });
 
 Footer.propTypes = {
-  onFilterChange: PropTypes.func.isRequired,
-  filter: PropTypes.oneOf([
-    'SHOW_ALL',
-    'SHOW_COMPLETED',
-    'SHOW_ACTIVE'
-  ]).isRequired
+	onFilterChange: PropTypes.func.isRequired,
+	filter: PropTypes.oneOf([
+		'SHOW_ALL',
+		'SHOW_COMPLETED',
+		'SHOW_ACTIVE'
+	]).isRequired
 }
 
 export default Footer;
--- app/containers/App.js
+++ app/containers/App.js
@@ -7,91 +7,91 @@ import TodoList from '../components/TodoList'
 import Footer from '../components/Footer'
 
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
 } = React;
 
 var ReactNativeReduxTodoApp = React.createClass({
 
-  render: function() {
-    const { dispatch, visibleTodos, visibilityFilter } = this.props;
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <AddTodo
-          onAddClick={text =>
-            dispatch(addTodo(text))
-          }
-        />
-        <TodoList
-          todos={visibleTodos}
-          onTodoClick={index =>
-            dispatch(completeTodo(index))
-          }
-        />
-        <Footer
-          filter={visibilityFilter}
-          onFilterChange={nextFilter =>
-            dispatch(setVisibilityFilter(nextFilter))
-          }
-        />
-      </View>
-    );
-  }
+	render: function() {
+		const { dispatch, visibleTodos, visibilityFilter } = this.props;
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>
+					Welcome to React Native!
+				</Text>
+				<AddTodo
+					onAddClick={text =>
+						dispatch(addTodo(text))
+					}
+				/>
+				<TodoList
+					todos={visibleTodos}
+					onTodoClick={index =>
+						dispatch(completeTodo(index))
+					}
+				/>
+				<Footer
+					filter={visibilityFilter}
+					onFilterChange={nextFilter =>
+						dispatch(setVisibilityFilter(nextFilter))
+					}
+				/>
+			</View>
+		);
+	}
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
 
 ReactNativeReduxTodoApp.propTypes = {
-  visibleTodos: React.PropTypes.arrayOf(
-    React.PropTypes.shape({
-      text: React.PropTypes.string.isRequired,
-      completed: React.PropTypes.bool.isRequired
-  })),
-  visibilityFilter: React.PropTypes.oneOf([
-    'SHOW_ALL',
-    'SHOW_COMPLETED',
-    'SHOW_ACTIVE'
-  ]).isRequired
+	visibleTodos: React.PropTypes.arrayOf(
+		React.PropTypes.shape({
+			text: React.PropTypes.string.isRequired,
+			completed: React.PropTypes.bool.isRequired
+	})),
+	visibilityFilter: React.PropTypes.oneOf([
+		'SHOW_ALL',
+		'SHOW_COMPLETED',
+		'SHOW_ACTIVE'
+	]).isRequired
 };
 
 function selectTodos(todos, filter) {
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return todos
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(todo => todo.completed)
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(todo => !todo.completed)
-  }
+	switch (filter) {
+		case VisibilityFilters.SHOW_ALL:
+			return todos
+		case VisibilityFilters.SHOW_COMPLETED:
+			return todos.filter(todo => todo.completed)
+		case VisibilityFilters.SHOW_ACTIVE:
+			return todos.filter(todo => !todo.completed)
+	}
 }
 
 function select(state) {
-  return {
-    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
-    visibilityFilter: state.visibilityFilter
-  }
+	return {
+		visibleTodos: selectTodos(state.todos, state.visibilityFilter),
+		visibilityFilter: state.visibilityFilter
+	}
 }
 
 export default connect(select)(ReactNativeReduxTodoApp);
